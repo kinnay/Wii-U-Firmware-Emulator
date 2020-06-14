44f2ba84f2fa98ad316f4aca56eae9b385dbdb70
James Benton <james.benton2@gmail.com>
2020-06-14 11:11:12 +0100

    typeutils: Fix variadic macros EnableIf / DisableIf.

--- src/common/typeutils.h
+++ src/common/typeutils.h
@@ -20,11 +20,11 @@ template <class T> using ReturnType = typename ReturnType_<T>::type;
 template <class T> using FirstArgument = typename FirstArgument_<T>::type;
 template <class T> using RemovePointer = typename std::remove_pointer<T>::type;
 
-#define EnableImpl(cond...) typename std::enable_if_t<cond> *
-#define DisableImpl(cond...) typename std::enable_if_t<!(cond)> *
+#define EnableImpl(...) typename std::enable_if_t<__VA_ARGS__> *
+#define DisableImpl(...) typename std::enable_if_t<!(__VA_ARGS__)> *
 
-#define EnableIf(cond...) EnableImpl(cond) = nullptr
-#define DisableIf(cond...) DisableImpl(cond) = nullptr
+#define EnableIf(...) EnableImpl(__VA_ARGS__) = nullptr
+#define DisableIf(...) DisableImpl(__VA_ARGS__) = nullptr
 
 #define IsEnum(T) std::is_enum<T>::value
 #define Convertible(T1, T2) std::is_convertible<T1, T2>::value
